1490649a72bdd665cd3d1acd85941cd2190b274a
speedycatfish <ymcacolin@gmail.com>
2018-11-16 15:32:03 -0800

    scrolling text

--- Hybrid Dating Sim/Hybrid Dating Sim/Assets/Scripts/TextBox.cs
+++ Hybrid Dating Sim/Hybrid Dating Sim/Assets/Scripts/TextBox.cs
@@ -1,16 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class TextBox : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+    string[] text = new string[5];
+    Text textComponent;
+    bool newText = false;
+    int index = 0;
+    float timeBetweenCharacters = 0.05f;
+    float lastTime;
+    int stringIndex = 0;
+    void Start () {
+        textComponent = gameObject.GetComponent<Text>();
+        text[0] = "sample text sample text sample text sample text sample text sample text ";
+        text[1] = "2 sample text";
+        ScrollText();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		if(newText == true && Time.time-lastTime >= timeBetweenCharacters)
+        {
+            if(stringIndex > text[index].Length)
+            {
+                newText = false;
+                index++;
+            }
+            else
+            {
+                textComponent.text = text[index].Substring(0, stringIndex);
+                stringIndex++;
+                lastTime = Time.time;
+            }
+        }
 	}
+    void ScrollText()
+    {
+        lastTime = Time.time;
+        newText = true;
+    }
 }
