fc92dca7cba03b6d4a0ce4b4e3e6f150ff01938c
speedycatfish <ymcacolin@gmail.com>
2018-11-16 19:43:53 -0800

    Added moving between text sections and skipping text
    Press z or left click to skip to the end of current text. Press z or left click again to go to next text section.

--- Hybrid Dating Sim/Hybrid Dating Sim/Assets/Scripts/TextBox.cs
+++ Hybrid Dating Sim/Hybrid Dating Sim/Assets/Scripts/TextBox.cs
@@ -2,40 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class TextBox : MonoBehaviour {
-    string[] text = new string[5];
-    Text textComponent;
-    bool newText = false;
-    int index = 0;
-    float timeBetweenCharacters = 0.05f;
-    float lastTime;
-    int stringIndex = 0;
-    void Start () {
+public class TextBox : MonoBehaviour
+{
+    string[] text = new string[5]; //array of text that people say
+    Text textComponent; //text component in text box
+    bool scrollingText = false; //is the text currently scrolling
+    bool displayText = false; //should text be displayed right now
+    int index = 0; //text array index
+    public float timeBetweenCharacters = 0.05f; //the time in seconds between letters being displayed.
+    float lastTime; //last time a new letter was displayed
+    int stringLength = 0; //length displayed of current text being displayed
+    void Start()
+    {
         textComponent = gameObject.GetComponent<Text>();
-        text[0] = "sample text sample text sample text sample text sample text sample text ";
-        text[1] = "2 sample text";
+        text[0] = "sample text sample text sample text  ";
+        text[1] = "2 sample text sample text sample text ";
+        text[2] = "3rd text sample text sample text ";
+        text[3] = "4th string sample text sample text ";
+        text[4] = "5 lorem ipsum sample text sample text ";
         ScrollText();
-	}
-	
-	void Update () {
-		if(newText == true && Time.time-lastTime >= timeBetweenCharacters)
+    }
+
+    void Update()
+    {
+        if (displayText == true)
         {
-            if(stringIndex > text[index].Length)
+            if (scrollingText == true && Time.time - lastTime >= timeBetweenCharacters)
             {
-                newText = false;
-                index++;
+                if (stringLength > text[index].Length)
+                {
+                    scrollingText = false;
+                    index++;
+                }
+                else
+                {
+                    textComponent.text = text[index].Substring(0, stringLength);
+                    stringLength++;
+                    lastTime = Time.time;
+                }
             }
-            else
+            if (Input.GetButtonDown("NextText"))
             {
-                textComponent.text = text[index].Substring(0, stringIndex);
-                stringIndex++;
-                lastTime = Time.time;
+                if (scrollingText == true)
+                {
+                    textComponent.text = text[index];
+                    scrollingText = false;
+                    index++;
+                }
+                else
+                {
+                    ScrollText();
+                }
             }
         }
-	}
+    }
     void ScrollText()
     {
+        displayText = true;
         lastTime = Time.time;
-        newText = true;
+        scrollingText = true;
+        stringLength = 0;
     }
 }
